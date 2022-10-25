9a7f750f2ff614d950938be3bb58b124e442076e
azizmohammad <fahimaziz.dev@gmail.com>
2022-10-25 18:46:34 +0600

    add to sarver site all code

--- index.js
+++ index.js
@@ -6,14 +6,41 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 const categories = require('./data/categoris.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('news api running')
 })
 
-
+// cetagoris data
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+
+// news data
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+
+// category news
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news)
+    }
+
 })
 
 
